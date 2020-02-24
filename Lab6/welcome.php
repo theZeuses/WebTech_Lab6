cc04acfcd1e924ccbe7cd903f953ec8cf4c2c1c4
theZeuses <38047515+theZeuses@users.noreply.github.com>
2020-02-24 20:56:20 +0600

    Add files via upload

--- Lab6/welcome.php
+++ Lab6/welcome.php
@@ -5,11 +5,11 @@ session_start();
 <!DOCTYPE html>
 <html>
 <body>
-
-	Name: <?php echo $_SESSION["nam"]; ?><br>
-	Email: <?php echo $_SESSION["nam"]; ?><br>
-	Gender: <?php echo $_SESSION["nam"]; ?><br>
-	Picture:<br> <?php echo "<img src=".$_SESSION["pic"].">"; ?>
+	<h2>Profile:</h2><br>
+	<b>Name:</b> <?php echo $_SESSION["nam"]; ?><br>
+	<b>Email:</b> <?php echo $_SESSION["em"]; ?><br>
+	<b>Gender:</b> <?php echo $_SESSION["gen"]; ?><br>
+	<b>Picture:</h3><b> <?php echo "<img src=".$_SESSION["pic"]." width=300 height=300>"; ?>
 
 </body>
 </html>
